7fd03974c52c76474c5d7dcdbfad5094f6f082fa
Roman N <naumenko_subscr@mail.ru>
2024-12-13 02:27:33 +0700

    Package

--- app/Http/Controllers/Posts/Index.php
+++ app/Http/Controllers/Posts/Index.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Posts;
 
 use App\Services\Repositories\PostsRepositoryInterface;
+use Bravik\MyPackage\CoolStaff;
 use Illuminate\View\View;
 
 class Index
@@ -15,8 +16,10 @@ class Index
     /**
      * Display a listing of the resource.
      */
-    public function __invoke(): View
+    public function __invoke(CoolStaff $coolStaff): View
     {
+//        dd($coolStaff->sayHi("John", "Doe"));
+
         $posts = $this->postsRepository->fetchAll();
 
         return view("posts.index", compact("posts"));
---
+++ config/my-package.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'enabled' => env("MY_PACKAGE_ENABLED", true),
+    'background-color' => 'yellow',
+];
