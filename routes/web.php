3a0b30092cf64ddba4d95c73c63bf61457787c01
Roman N <naumenko_subscr@mail.ru>
2025-05-22 15:09:57 +0700

    Сгенерировали примитивный ресурсный контроллер и добавили маршрут

--- routes/web.php
+++ routes/web.php
@@ -9,30 +9,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/**
- * Пример группировки маршрутов
- */
-Route::group([
-    'prefix' => 'posts',                                // Префикс для URL маршрутов
-    'middleware' => ['auth', 'canany:admin,editor'],    // На всю группу маршрутов можно навесить middlewares
-    'as' => 'posts.',                                   // Префикс для имен маршрутов
-], static function () {
-    // Полное имя будет: posts.index
-    // URL будет: /posts
-    Route::get('/', static fn () => 'Dummy response')
-        ->name('index');
 
-    // Аналогично в остальных
-    Route::get('create', static fn () => 'Dummy response')
-        ->name('create');
-    Route::post('/', static fn () => 'Dummy response')
-        ->name('store');
-    Route::get('{postId}', static fn () => 'Dummy response')
-        ->name('show');
-    Route::get('{post}/edit', static fn () => 'Dummy response')
-        ->name('edit');
-    Route::put('{postId}', static fn () => 'Dummy response')
-        ->name('update');
-    Route::delete('{post}', static fn () => 'Dummy response')
-        ->name('destroy');
-});
+Route::resource('posts', \App\Http\Controllers\PostsController::class);
+
+///**
+// * Пример группировки маршрутов
+// */
+//Route::group([
+//    'prefix' => 'posts',                                // Префикс для URL маршрутов
+//    'middleware' => ['auth', 'canany:admin,editor'],    // На всю группу маршрутов можно навесить middlewares
+//    'as' => 'posts.',                                   // Префикс для имен маршрутов
+//], static function () {
+//    // Полное имя будет: posts.index
+//    // URL будет: /posts
+//    Route::get('/', static fn () => 'Dummy response')
+//        ->name('index');
+//
+//    // Аналогично в остальных
+//    Route::get('create', static fn () => 'Dummy response')
+//        ->name('create');
+//    Route::post('/', static fn () => 'Dummy response')
+//        ->name('store');
+//    Route::get('{postId}', static fn () => 'Dummy response')
+//        ->name('show');
+//    Route::get('{post}/edit', static fn () => 'Dummy response')
+//        ->name('edit');
+//    Route::put('{postId}', static fn () => 'Dummy response')
+//        ->name('update');
+//    Route::delete('{post}', static fn () => 'Dummy response')
+//        ->name('destroy');
+//});
